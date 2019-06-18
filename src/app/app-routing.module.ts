e338ba31d250d971c07b385015bbbbdd63eed04d
dilan <dilanlahiruijse@gmail.com>
2019-06-18 15:37:55 +0530

    added some screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './view/dashboard/dashboard.component';
+import { ManageCustomersComponent } from './view/manage-customers/manage-customers.component';
+import { ManageItemesComponent } from './view/manage-itemes/manage-itemes.component';
 
 const routes: Routes = [
 
@@ -9,10 +11,18 @@ const routes: Routes = [
     component: DashboardComponent
   },
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'dashboard'
-  }
+    path: 'customers',
+    component: ManageCustomersComponent
+  },
+  {
+    path: 'items',
+    component: ManageItemesComponent
+  },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'dashboard'
+  // }
 ];
 
 @NgModule({
